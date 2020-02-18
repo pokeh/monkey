3e3f8c1fabbbe0dce0fec9fc0c2a69d35fc02fac
Chiyo T N <chiyo.nishio@gmail.com>
2020-02-17 17:29:35 -0800

    Add REPL

---
+++ repl/repl.go
@@ -0,0 +1,33 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/pokeh/monkey/lexer"
+	"github.com/pokeh/monkey/token"
+)
+
+const PROMPT = "⊂((*'⊥'*))⊃ >>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			// %v -> the value in a default format when printing structs,
+			// the plus flag (%+v) adds field names
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
